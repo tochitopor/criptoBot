573a51bc8a3741054a5c9cf47c410c797708cdca
IAGanja <abichaika@yandex.ru>
2021-11-11 14:45:40 +0300

    cb-0: added SpringBoot skeleton to the project.

---
+++ src/test/java/com/github/tochitopor/criptoBot/CriptoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tochitopor.criptoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
